74e16e938e36aab1923e821d95efe905b29ee1bd
yaserbesir <yaserbesir7@gmail.com>
2026-03-09 03:35:02 +0300

    Update factors.js

--- factors.js
+++ factors.js
@@ -1,22 +1,15 @@
 module.exports = {
-
 naturalGas:2.02,
 diesel:2.68,
 gasoline:2.31,
 lpg:1.51,
 coal:2.42,
 fuelOil:3.15,
-
 electricity:0.442,
-
 freightRoad:0.062,
 freightSea:0.016,
 freightAir:0.602,
-
 hotelNights:15.5,
-
 paperUsage:0.94,
-
 waterSupply:0.000344
-
 }
